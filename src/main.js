c906a3546049668855420c7f54a0c58650ad46f7
Chung Leong <chernyshevsky@hotmail.com>
2019-03-19 09:00:50 -0400

    Renamed container node.

--- src/main.js
+++ src/main.js
@@ -5,10 +5,7 @@ import { FrontEnd } from 'front-end';
 window.addEventListener('load', initialize);
 
 function initialize(evt) {
-    let appContainer = document.getElementById('app-container');
-    if (!appContainer) {
-        throw new Error('Unable to find app element in DOM');
-    }
-    let appElement = createElement(FrontEnd, {});
-    render(appElement, appContainer);
+    let container = document.getElementById('react-container');
+    let element = createElement(FrontEnd, {});
+    render(element, container);
 }
